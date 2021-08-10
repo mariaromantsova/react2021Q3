7c3ad26baf4d1102bb42de664c252262a1073128
flowerpower888 <oingooboingo@gmail.com>
2021-08-10 17:32:54 +0300

    fix: fix animation

--- react-routing/src/App.tsx
+++ react-routing/src/App.tsx
@@ -1,36 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
-import Navbar from './components/Navbar/Navbar';
+import {
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter,
+} from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
 import routes from './routes';
 
-const App: React.FunctionComponent = () => {
+const App: React.FunctionComponent<RouteComponentProps> = ({ location }) => {
   return (
-    <Router>
-      <Navbar />
-
-      <Switch>
-        {routes.map(({ path, Component }) => (
-          <Route key={path} exact path={path}>
-            {({ match }) => (
-              <CSSTransition
-                in={match != null}
-                timeout={300}
-                classNames="page"
-                unmountOnExit
-              >
-                <div className="page container mt-5 d-flex flex-column justify-content-center align-items-center">
-                  <Component />
-                </div>
-              </CSSTransition>
-            )}
-          </Route>
-        ))}
-        <Route exact component={ErrorPage} />
-      </Switch>
-    </Router>
+    <TransitionGroup component={null}>
+      <CSSTransition key={location.key} timeout={300} classNames="page">
+        <div className="page container mt-5 d-flex flex-column justify-content-center align-items-center">
+          <Switch location={location}>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} exact path={path}>
+                <Component />
+              </Route>
+            ))}
+            <Route exact component={ErrorPage} />
+          </Switch>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
-export default App;
+export default withRouter(App);
--- react-routing/src/pages/ErrorPage/ErrorPage.tsx
+++ react-routing/src/pages/ErrorPage/ErrorPage.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage: React.FunctionComponent = () => {
   return (
-    <div className="page container mt-5 d-flex flex-column justify-content-center align-items-center">
+    <>
       <h1>404 - Not Found</h1>
       <Link to="/" className="link-dark">
         Go Home
       </Link>
-    </div>
+    </>
   );
 };
 
